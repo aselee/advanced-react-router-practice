5bbe52c7cc9c4535837a972311e3f4bd0f1ad4dd
Andrew <32229265+aselee@users.noreply.github.com>
2018-06-15 14:10:08 -0500

    small edit

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Marquee(props) {
   const message = props.match.params.text;
   return (   
     <marquee>{message}</marquee>
+    <Link to={"/marquee/iloveroutes"}> View </Link>
   );
 }
 
